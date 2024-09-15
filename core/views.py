ced2b277a61609075d2c0160ce49d3ddb4f279a1
Pious Ackon <ackonpious469@gmail.com>
2024-09-15 11:28:49 +0000

    Add new files and update existing ones for item and category sections
    - Added compiled Python files in __pycache__ directories.
    - Updated `core/templates/core/index.html` and `core/views.py`.
    - Modified `db.sqlite3` with latest changes.
    - Added compiled Python files in `ecommence/__pycache__`.
    - Updated `ecommence/urls.py`.
    - Added compiled Python files in `item/__pycache__` and `item/migrations/__pycache__`.
    - Updated `item/views.py`.
    - Added new image file to `media/Image_items/`.

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from item.models import Item, Categroy
 
 # Create your views here.
 def index(request):
-    return render(request, "core/index.html")
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categroies = Categroy.objects.all()
+    return render(request, "core/index.html", {
+        'categroies' : categroies,
+        'items' : items,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
