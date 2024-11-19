69737ed2a5c23c426b1481690a866639b91540ab
AckonPious <ackonpious469@gmail.com>
2024-11-19 14:43:53 +0000

    update [authentication] url and views

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import view
+from django.views.generic import View
 
 
-class HomePageView(view):
+class HomePageView(View):
     template_name = 'core/home.html'
     
     def get(self, request, *args, **kwargs):
