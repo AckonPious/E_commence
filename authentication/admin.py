30d2e4e511e516e596770182ebbc065d6a0e124b
AckonPious <ackonpious469@gmail.com>
2024-11-29 10:56:52 +0000

    Remove ignored files from repository

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ['username', 'email', 'is_admin']
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None,{'fields' :('is_admin')})
+    )
+    list_filter = ['username', 'email', 'is_admin']
+    search_fields = ['username']
+    
+admin.site.register(User, UserAdmin)
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    username = models.CharField(max_length=150, unique=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS = ['username'] 
+    
+    def save(self, *args, **kwargs):
+        if not self.username and self.email:
+            self.username = self.email.split('@')[0]
+        super().self(*args, **kwargs)
+        
+    class Meta:
+        ordering =  ['username']
+        
+    def __str__(self):
+        return self.username
+
+  
